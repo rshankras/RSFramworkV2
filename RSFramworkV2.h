12de9894ad1e273f992c373db3361e7f1d55c69f
Ravi Shankar <ravishankar@outlook.in>
2024-10-07 20:35:19 +0530

    Initial commit

---
+++ RSFramworkV2.h
@@ -0,0 +1,18 @@
+//
+//  RSFramworkV2.h
+//  RSFramworkV2
+//
+//  Created by Ravi Shankar on 07/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RSFramworkV2.
+FOUNDATION_EXPORT double RSFramworkV2VersionNumber;
+
+//! Project version string for RSFramworkV2.
+FOUNDATION_EXPORT const unsigned char RSFramworkV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSFramworkV2/PublicHeader.h>
+
+
